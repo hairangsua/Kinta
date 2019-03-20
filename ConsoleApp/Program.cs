deba2374e3d02a1bae38c77af377677e3356d79a
Hai Hoang Phi <hoangphihai93@gmail.com>
2019-03-20 21:56:35 +0700

    Update Program.cs
    merge

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,11 +8,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             ValidConsoleInput.Run();
-=======
-            TestTachTuanTheoThang.Run();
->>>>>>> 886abd370d3807b043639b1bc4ba6230c7e47743
         }
 
 
